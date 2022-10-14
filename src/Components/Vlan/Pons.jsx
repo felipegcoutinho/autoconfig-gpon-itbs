ee430d1b697ecdb6e70dd154af20cf9bb5ccf1c9
Felipe Coutinho <felipi@live.com>
2022-10-14 09:55:09 -0300

    update forms

--- src/Components/Vlan/Pons.jsx
+++ src/Components/Vlan/Pons.jsx
@@ -9,78 +9,77 @@ export default function Pons() {
   return (
     <div id="aim" className={Style.container}>
       <h2> Definição dos AIMs do Profile VLAN</h2>
-
-      <div className={Style.content}>
+      <form className={Style.content} onChange={handleChange} >
         <div className={Style.pons}>
-          <Aim name="aimvlanpon1" value={values.aimvlanpon1} title="PON 1" handleChange={handleChange} />
-          <Vlans name="vlanpon1" value={values.vlanpon1} handleChange={handleChange} />
+          <Aim name="aimvlanpon1" defaultValue={values.aimvlanpon1} title="PON 1" />
+          <Vlans name="vlanpon1" defaultValue={values.vlanpon1} />
         </div>
         <div className={Style.pons}>
-          <Aim name="aimvlanpon2" value={values.aimvlanpon2} title="PON 2" handleChange={handleChange} />
-          <Vlans name="vlanpon2" value={values.vlanpon2} handleChange={handleChange} />
+          <Aim name="aimvlanpon2" defaultValue={values.aimvlanpon2} title="PON 2" />
+          <Vlans name="vlanpon2" defaultValue={values.vlanpon2} />
         </div>
         <div className={Style.pons}>
-          <Aim name="aimvlanpon3" value={values.aimvlanpon3} title="PON 3" handleChange={handleChange} />
-          <Vlans name="vlanpon3" value={values.vlanpon3} handleChange={handleChange} />
+          <Aim name="aimvlanpon3" defaultValue={values.aimvlanpon3} title="PON 3" />
+          <Vlans name="vlanpon3" defaultValue={values.vlanpon3} />
         </div>
         <div className={Style.pons}>
-          <Aim name="aimvlanpon4" value={values.aimvlanpon4} title="PON 4" handleChange={handleChange} />
-          <Vlans name="vlanpon4" value={values.vlanpon4} handleChange={handleChange} />
+          <Aim name="aimvlanpon4" defaultValue={values.aimvlanpon4} title="PON 4" />
+          <Vlans name="vlanpon4" defaultValue={values.vlanpon4} />
         </div>
         <div className={Style.pons}>
-          <Aim name="aimvlanpon5" value={values.aimvlanpon5} title="PON 5" handleChange={handleChange} />
-          <Vlans name="vlanpon5" value={values.vlanpon5} handleChange={handleChange} />
+          <Aim name="aimvlanpon5" defaultValue={values.aimvlanpon5} title="PON 5" />
+          <Vlans name="vlanpon5" defaultValue={values.vlanpon5} />
         </div>
         <div className={Style.pons}>
-          <Aim name="aimvlanpon6" value={values.aimvlanpon6} title="PON 6" handleChange={handleChange} />
-          <Vlans name="vlanpon6" value={values.vlanpon6} handleChange={handleChange} />
+          <Aim name="aimvlanpon6" defaultValue={values.aimvlanpon6} title="PON 6" />
+          <Vlans name="vlanpon6" defaultValue={values.vlanpon6} />
         </div>
         <div className={Style.pons}>
-          <Aim name="aimvlanpon7" value={values.aimvlanpon7} title="PON 7" handleChange={handleChange} />
-          <Vlans name="vlanpon7" value={values.vlanpon7} handleChange={handleChange} />
+          <Aim name="aimvlanpon7" defaultValue={values.aimvlanpon7} title="PON 7" />
+          <Vlans name="vlanpon7" defaultValue={values.vlanpon7} />
         </div>
         <div className={Style.pons}>
-          <Aim name="aimvlanpon8" value={values.aimvlanpon8} title="PON 8" handleChange={handleChange} />
-          <Vlans name="vlanpon8" value={values.vlanpon8} handleChange={handleChange} />
+          <Aim name="aimvlanpon8" defaultValue={values.aimvlanpon8} title="PON 8" />
+          <Vlans name="vlanpon8" defaultValue={values.vlanpon8} />
         </div>
         {checked && (
           <>
             <div className={Style.pons}>
-              <Aim name="aimvlanpon9" value={values.aimvlanpon9} title="PON 9" handleChange={handleChange} />
-              <Vlans name="vlanpon9" value={values.vlanpon9} handleChange={handleChange} />
+              <Aim name="aimvlanpon9" defaultValue={values.aimvlanpon9} title="PON 9" />
+              <Vlans name="vlanpon9" defaultValue={values.vlanpon9} />
             </div>
             <div className={Style.pons}>
-              <Aim name="aimvlanpon10" value={values.aimvlanpon10} title="PON 10" handleChange={handleChange} />
-              <Vlans name="vlanpon10" value={values.vlanpon10} handleChange={handleChange} />
+              <Aim name="aimvlanpon10" defaultValue={values.aimvlanpon10} title="PON 10" />
+              <Vlans name="vlanpon10" defaultValue={values.vlanpon10} />
             </div>
             <div className={Style.pons}>
-              <Aim name="aimvlanpon11" value={values.aimvlanpon11} title="PON 11" handleChange={handleChange} />
-              <Vlans name="vlanpon11" value={values.vlanpon11} handleChange={handleChange} />
+              <Aim name="aimvlanpon11" defaultValue={values.aimvlanpon11} title="PON 11" />
+              <Vlans name="vlanpon11" defaultValue={values.vlanpon11} />
             </div>
             <div className={Style.pons}>
-              <Aim name="aimvlanpon12" value={values.aimvlanpon12} title="PON 12" handleChange={handleChange} />
-              <Vlans name="vlanpon12" value={values.vlanpon12} handleChange={handleChange} />
+              <Aim name="aimvlanpon12" defaultValue={values.aimvlanpon12} title="PON 12" />
+              <Vlans name="vlanpon12" defaultValue={values.vlanpon12} />
             </div>
             <div className={Style.pons}>
-              <Aim name="aimvlanpon13" value={values.aimvlanpon13} title="PON 13" handleChange={handleChange} />
-              <Vlans name="vlanpon13" value={values.vlanpon13} handleChange={handleChange} />
+              <Aim name="aimvlanpon13" defaultValue={values.aimvlanpon13} title="PON 13" />
+              <Vlans name="vlanpon13" defaultValue={values.vlanpon13} />
             </div>
             <div className={Style.pons}>
-              <Aim name="aimvlanpon14" value={values.aimvlanpon14} title="PON 14" handleChange={handleChange} />
-              <Vlans name="vlanpon14" value={values.vlanpon14} handleChange={handleChange} />
+              <Aim name="aimvlanpon14" defaultValue={values.aimvlanpon14} title="PON 14" />
+              <Vlans name="vlanpon14" defaultValue={values.vlanpon14} />
             </div>
             <div className={Style.pons}>
-              <Aim name="aimvlanpon15" value={values.aimvlanpon15} title="PON 15" handleChange={handleChange} />
-              <Vlans name="vlanpon15" value={values.vlanpon15} handleChange={handleChange} />
+              <Aim name="aimvlanpon15" defaultValue={values.aimvlanpon15} title="PON 15" />
+              <Vlans name="vlanpon15" defaultValue={values.vlanpon15} />
             </div>
             <div className={Style.pons}>
-              <Aim name="aimvlanpon16" value={values.aimvlanpon16} title="PON 16" handleChange={handleChange} />
-              <Vlans name="vlanpon16" value={values.vlanpon16} handleChange={handleChange} />
+              <Aim name="aimvlanpon16" defaultValue={values.aimvlanpon16} title="PON 16" />
+              <Vlans name="vlanpon16" defaultValue={values.vlanpon16} />
             </div>
           </>
         )}
-      </div>
-    </div >
+      </form>
+    </div>
   )
 }
 
