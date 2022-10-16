55c294d621e7ee1e292706549e40a952b7138016
Felipe Coutinho <felipi@live.com>
2022-10-16 18:04:51 -0300

    update header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,12 +9,12 @@ export default function header() {
           <li><a className={Style.title} href="https://github.com/felipegcoutinho">GPON ITBS <span className={Style.version}>v1.0</span> </a></li>
           <li><a href="#">OLT G08 <i className="fa-solid fa-caret-down"></i></a>
             <ul>
-              <li><a href="#"><i className="fa-solid fa-caret-right"></i> Manual</a></li>
+              <li><a href="https://olts-guias-e-manuais.intelbras.com.br/G08/index.html"><i className="fa-solid fa-caret-right"></i> Manual e Guia</a></li>
             </ul>
           </li>
           <li><a href="#">OLT G16 <i className="fa-solid fa-caret-down"></i></a>
             <ul>
-              <li><a href="#"><i className="fa-solid fa-caret-right"></i> Manual</a></li>
+              <li><a href="https://olts-guias-e-manuais.intelbras.com.br/G16/index.html"><i className="fa-solid fa-caret-right"></i> Manual e Guia</a></li>
             </ul>
           </li>
           <li><a target="_blank" rel="noopener noreferrer" href="https://8820i.felipecoutinho.com/"> OLT 8820i <i className="fa-solid fa-sm fa-arrow-up-right-from-square"></i></a></li>
