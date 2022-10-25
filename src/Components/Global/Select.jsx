568f7338df873d187c658f26f3e16ea4cf754ebb
Felipe Coutinho <felipi@live.com>
2022-10-25 11:05:43 -0300

    update design details

--- src/Components/Global/Select.jsx
+++ src/Components/Global/Select.jsx
@@ -13,19 +13,13 @@ export default function Select() {
     <div className={Style.content}>
       <h2 className={Style.h2}>Escolha o Concentrador que será configurado:</h2>
       <nav className={Style.containerBtn}>
-        <NavLink to="/G08" onClick={G08} className={Style.btn}
-          style={({ isActive }) => ({
-            color: isActive ? '#fff' : '#2a2f37',
-            background: isActive ? '#2a2f37' : '#f0f0f0',
-            border: isActive ? '2px solid #f0f0f0' : '2px solid #f0f0f0',
-          })}>OLT G08</NavLink>
+        <NavLink to="/G08" onClick={G08} className={({ isActive }) => (isActive ? Style.btnActive : Style.btnNotActive)}>
+          OLT G08
+        </NavLink>
 
-        <NavLink to="/G16" onClick={G16} className={Style.btn}
-          style={({ isActive }) => ({
-            color: isActive ? '#fff' : '#2a2f37',
-            background: isActive ? '#2a2f37' : '#f0f0f0',
-            border: isActive ? '2px solid #f0f0f0' : '2px solid #f0f0f0',
-          })}>OLT G16</NavLink>
+        <NavLink to="/G16" onClick={G16} className={({ isActive }) => (isActive ? Style.btnActive : Style.btnNotActive)}>
+          OLT G16
+        </NavLink>
       </nav>
     </div >
   )
