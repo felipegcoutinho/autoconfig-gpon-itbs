4ca7a9736eb9b384dbf5b2f173fa28122f88a3e2
Felipe Coutinho <felipi@live.com>
2022-10-25 16:44:46 -0300

    add footer

---
+++ src/Components/Global/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Style from './Footer.module.css';
+
+export default function header() {
+  return (
+    <div id="start" className={Style.container}>
+      TESTE
+    </div>
+  );
+}
