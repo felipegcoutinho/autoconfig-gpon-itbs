cb8a1bf1d865e775f3a40420efae58251e63222e
Felipe Coutinho <felipi@live.com>
2022-10-26 00:20:31 -0300

    add favicon

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Style from './Footer.module.css';
 
-export default function header() {
+export default function Footer() {
   return (
     <div className={Style.container}>
       <div className={Style.content}>
-        <a className={Style.site} href="#">FELIPECOUTINHO.COM</a>
+        <a className={Style.site} href="https://felipecoutinho.com">FELIPECOUTINHO.COM</a>
         {'|'}
-        <a className={Style.github} href="#">GITHUB.FELIPECOUTINHO.COM</a>
+        <a className={Style.github} href="https://github.felipecoutinho.com">GITHUB.FELIPECOUTINHO.COM</a>
       </div>
     </div>
   );
