fc874caf3d6c27ba4dc28637f3f690f1d4eb3990
Felipe Coutinho <felipi@live.com>
2023-07-26 19:04:48 -0300

    add 1800 onts

--- src/js/DeviceNames.js
+++ src/js/DeviceNames.js
@@ -1,57 +1,61 @@
-import React from 'react'
-import { ValueContext } from "./Context";
+import React from "react";
+import {ValueContext} from "./Context";
 import ServiceContext from "./Context";
 
 //DEVICE BRIDGE
 export function DeviceNameBridge() {
+  const {values} = React.useContext(ValueContext);
 
-  const { values } = React.useContext(ValueContext);
-
-  if (values.devicebridge == 'i30-100') {
-    return '110Gi';
+  if (values.devicebridge == "i30-100") {
+    return "110Gi";
   }
-  if (values.devicebridge == 'i41-100') {
-    return '110Gb';
+  if (values.devicebridge == "i41-100") {
+    return "110Gb";
   }
-  if (values.devicebridge == 'i40-100') {
-    return 'R1';
+  if (values.devicebridge == "i40-100") {
+    return "R1";
   }
-  if (values.devicebridge == 'i40-100-v2') {
-    return 'R1v2';
+  if (values.devicebridge == "i40-100-v2") {
+    return "R1v2";
   }
 }
 
 //DEVICE ROUTER
 export function DeviceNameRouter() {
+  const {values} = React.useContext(ValueContext);
 
-  const { values } = React.useContext(ValueContext);
-
-  if (values.devicerouter == 'i40-100') {
-    return 'R1';
+  if (values.devicerouter == "i40-100") {
+    return "R1";
   }
-  if (values.devicerouter == 'i40-100-v2') {
-    return 'R1v2';
+  if (values.devicerouter == "i40-100-v2") {
+    return "R1v2";
   }
-  if (values.devicerouter == 'i10-420') {
-    return '1420G';
+  if (values.devicerouter == "i10-420") {
+    return "1420G";
   }
-  if (values.devicerouter == 'i40-421') {
-    return '142NW';
+  if (values.devicerouter == "i40-421") {
+    return "142NW";
   }
-  if (values.devicerouter == 'i41-421') {
-    return '142NG';
+  if (values.devicerouter == "i41-421") {
+    return "142NG";
   }
-  if (values.devicerouter == 'i40-211') {
-    return '121W';
+  if (values.devicerouter == "i40-211") {
+    return "121W";
   }
-  if (values.devicerouter == 'i41-211') {
-    return '121AC';
+  if (values.devicerouter == "i41-211") {
+    return "121AC";
   }
-  if (values.devicerouter == 'i40-201') {
-    return '120AC';
+  if (values.devicerouter == "i40-201") {
+    return "120AC";
   }
-  if (values.devicerouter == 'i41-201') {
-    return '1200R';
+  if (values.devicerouter == "i41-201") {
+    return "1200R";
+  }
+  if (values.devicerouter == "i40-401") {
+    return "1800";
+  }
+  if (values.devicerouter == "i40-411") {
+    return "1800v";
   }
 }
 
@@ -59,53 +63,57 @@ export function DeviceNameRouter() {
 
 //DEVICE BRIDGE
 export function DeviceNamesBridge() {
+  const {values} = React.useContext(ServiceContext);
 
-  const { values } = React.useContext(ServiceContext);
-
-  if (values.devicebridge == 'i30-100') {
-    return '110Gi';
+  if (values.devicebridge == "i30-100") {
+    return "110Gi";
   }
-  if (values.devicebridge == 'i41-100') {
-    return '110Gb';
+  if (values.devicebridge == "i41-100") {
+    return "110Gb";
   }
-  if (values.devicebridge == 'i40-100') {
-    return 'R1';
+  if (values.devicebridge == "i40-100") {
+    return "R1";
   }
-  if (values.devicebridge == 'i40-100-v2') {
-    return 'R1v2';
+  if (values.devicebridge == "i40-100-v2") {
+    return "R1v2";
   }
 }
 
 //DEVICE ROUTER
 export function DeviceNamesRouter() {
+  const {values} = React.useContext(ServiceContext);
 
-  const { values } = React.useContext(ServiceContext);
-
-  if (values.devicerouter == 'i40-100') {
-    return 'R1';
+  if (values.devicerouter == "i40-100") {
+    return "R1";
   }
-  if (values.devicerouter == 'i40-100-v2') {
-    return 'R1v2';
+  if (values.devicerouter == "i40-100-v2") {
+    return "R1v2";
   }
-  if (values.devicerouter == 'i10-420') {
-    return '1420G';
+  if (values.devicerouter == "i10-420") {
+    return "1420G";
   }
-  if (values.devicerouter == 'i40-421') {
-    return '142NW';
+  if (values.devicerouter == "i40-421") {
+    return "142NW";
   }
-  if (values.devicerouter == 'i41-421') {
-    return '142NG';
+  if (values.devicerouter == "i41-421") {
+    return "142NG";
   }
-  if (values.devicerouter == 'i40-211') {
-    return '121W';
+  if (values.devicerouter == "i40-211") {
+    return "121W";
   }
-  if (values.devicerouter == 'i41-211') {
-    return '121AC';
+  if (values.devicerouter == "i41-211") {
+    return "121AC";
   }
-  if (values.devicerouter == 'i40-201') {
-    return '120AC';
+  if (values.devicerouter == "i40-201") {
+    return "120AC";
   }
-  if (values.devicerouter == 'i41-201') {
-    return '1200R';
+  if (values.devicerouter == "i41-201") {
+    return "1200R";
+  }
+  if (values.devicerouter == "i40-401") {
+    return "1800";
+  }
+  if (values.devicerouter == "i40-411") {
+    return "1800v";
   }
 }
--- src/js/DeviceOptions.js
+++ src/js/DeviceOptions.js
@@ -1,202 +1,210 @@
 export const DeviceOptionsBridge = [
   {
-    key: '1',
-    label: '110Gi',
-    value: 'i30-100',
+    key: "1",
+    label: "110Gi",
+    value: "i30-100",
   },
   {
-    key: '2',
-    label: '110Gb',
-    value: 'i41-100',
+    key: "2",
+    label: "110Gb",
+    value: "i41-100",
   },
   {
-    key: '3',
-    label: 'R1',
-    value: 'i40-100',
+    key: "3",
+    label: "R1",
+    value: "i40-100",
   },
   {
-    key: '4',
-    label: 'R1v2',
-    value: 'i40-100-v2',
+    key: "4",
+    label: "R1v2",
+    value: "i40-100-v2",
   },
 ];
 
-
 export const DeviceOptionsRouter = [
   {
-    key: '1',
-    label: 'R1',
-    value: 'i40-100',
+    key: "1",
+    label: "R1",
+    value: "i40-100",
   },
   {
-    key: '2',
-    label: 'R1v2',
-    value: 'i40-100-v2',
+    key: "2",
+    label: "R1v2",
+    value: "i40-100-v2",
   },
   {
-    key: '3',
-    label: '1420G',
-    value: 'i10-420',
+    key: "3",
+    label: "1420G",
+    value: "i10-420",
   },
   {
-    key: '4',
-    label: '142NW',
-    value: 'i40-421',
+    key: "4",
+    label: "142NW",
+    value: "i40-421",
   },
   {
-    key: '5',
-    label: '142NG',
-    value: 'i41-421',
+    key: "5",
+    label: "142NG",
+    value: "i41-421",
   },
   {
-    key: '6',
-    label: '121W',
-    value: 'i40-211',
+    key: "6",
+    label: "121W",
+    value: "i40-211",
   },
   {
-    key: '7',
-    label: '121AC',
-    value: 'i41-211',
+    key: "7",
+    label: "121AC",
+    value: "i41-211",
   },
   {
-    key: '8',
-    label: '120AC',
-    value: 'i40-201',
+    key: "8",
+    label: "120AC",
+    value: "i40-201",
   },
   {
-    key: '9',
-    label: '1200R',
-    value: 'i41-201',
+    key: "9",
+    label: "1200R",
+    value: "i41-201",
+  },
+  {
+    key: "10",
+    label: "AX 1800",
+    value: "i40-401",
+  },
+  {
+    key: "11",
+    label: "AX 1800 V",
+    value: "i40-411",
   },
 ];
 
 export const OnuOptions = [
   {
-    key: '1',
-    label: 'bridge',
-    value: 'bridge',
+    key: "1",
+    label: "bridge",
+    value: "bridge",
   },
   {
-    key: '2',
-    label: 'router',
-    value: 'router',
+    key: "2",
+    label: "router",
+    value: "router",
   },
-
-]
+];
 
 export const BridgeOptions = [
   {
-    key: '1',
-    label: 'uplink',
-    value: 'uplink',
+    key: "1",
+    label: "uplink",
+    value: "uplink",
   },
   {
-    key: '2',
-    label: 'intralink',
-    value: 'intralink',
+    key: "2",
+    label: "intralink",
+    value: "intralink",
   },
   {
-    key: '3',
-    label: 'tls',
-    value: 'tls',
+    key: "3",
+    label: "tls",
+    value: "tls",
   },
-]
+];
 
 export const ModoVlanOptions = [
   {
-    key: '1',
-    label: 'tagged',
-    value: 'tagged',
+    key: "1",
+    label: "tagged",
+    value: "tagged",
   },
   {
-    key: '2',
-    label: 'untagged',
-    value: 'untagged',
+    key: "2",
+    label: "untagged",
+    value: "untagged",
   },
-]
+];
 
 export const UplinkOptions = [
   {
-    key: '1',
-    label: 'eth 1',
-    value: 'eth 1',
+    key: "1",
+    label: "eth 1",
+    value: "eth 1",
   },
   {
-    key: '2',
-    label: 'eth 2',
-    value: 'eth 2',
+    key: "2",
+    label: "eth 2",
+    value: "eth 2",
   },
   {
-    key: '3',
-    label: 'eth 3',
-    value: 'eth 3',
+    key: "3",
+    label: "eth 3",
+    value: "eth 3",
   },
   {
-    key: '4',
-    label: 'eth 4',
-    value: 'eth 4',
+    key: "4",
+    label: "eth 4",
+    value: "eth 4",
   },
   {
-    key: '5',
-    label: 'eth 5',
-    value: 'eth 5',
+    key: "5",
+    label: "eth 5",
+    value: "eth 5",
   },
   {
-    key: '6',
-    label: 'eth 6',
-    value: 'eth 6',
+    key: "6",
+    label: "eth 6",
+    value: "eth 6",
   },
   {
-    key: '7',
-    label: 'eth 7',
-    value: 'eth 7',
+    key: "7",
+    label: "eth 7",
+    value: "eth 7",
   },
   {
-    key: '8',
-    label: 'eth 8',
-    value: 'eth 8',
+    key: "8",
+    label: "eth 8",
+    value: "eth 8",
   },
   {
-    key: '9',
-    label: 'xeth 1',
-    value: 'xeth 1',
+    key: "9",
+    label: "xeth 1",
+    value: "xeth 1",
   },
   {
-    key: '10',
-    label: 'xeth 2',
-    value: 'xeth 2',
+    key: "10",
+    label: "xeth 2",
+    value: "xeth 2",
   },
 ];
 
 export const UplinkOptionsG16 = [
   {
-    key: '1',
-    label: 'ethernet 1/1',
-    value: 'ethernet 1/1',
+    key: "1",
+    label: "ethernet 1/1",
+    value: "ethernet 1/1",
   },
   {
-    key: '2',
-    label: 'ethernet 1/2',
-    value: 'ethernet 1/2',
+    key: "2",
+    label: "ethernet 1/2",
+    value: "ethernet 1/2",
   },
   {
-    key: '3',
-    label: 'ethernet 1/3',
-    value: 'ethernet 1/3',
+    key: "3",
+    label: "ethernet 1/3",
+    value: "ethernet 1/3",
   },
   {
-    key: '4',
-    label: 'ethernet 1/4',
-    value: 'ethernet 1/4',
+    key: "4",
+    label: "ethernet 1/4",
+    value: "ethernet 1/4",
   },
   {
-    key: '5',
-    label: 'ethernet 2/1 (SFP+)',
-    value: 'ethernet 2/1',
+    key: "5",
+    label: "ethernet 2/1 (SFP+)",
+    value: "ethernet 2/1",
   },
   {
-    key: '6',
-    label: 'ethernet 2/2 (SFP+)',
-    value: 'ethernet 2/2',
+    key: "6",
+    label: "ethernet 2/2 (SFP+)",
+    value: "ethernet 2/2",
   },
 ];
